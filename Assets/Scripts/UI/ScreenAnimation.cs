7fdbc4ed79e06a640784a8f9c983605872237da4
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-02-05 15:53:55 +0400

    AddNewShop

--- Assets/Scripts/UI/ScreenAnimation.cs
+++ Assets/Scripts/UI/ScreenAnimation.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 public class ScreenAnimation : MonoBehaviour
 {
     [SerializeField] private RectTransform _screen;
-    [SerializeField] private float animationDuration = 1.0f;
+    [SerializeField] private float animationDuration = 0.15f;
     private Vector2 offScreenPosition;
     private Vector2 onScreenPosition;
     
     private void OnEnable()
     {
-        offScreenPosition = new Vector2(0, -Screen.height*3);
+        offScreenPosition = new Vector2(0, -Screen.height*1.5f);
         onScreenPosition = new Vector2(0, 0);
         _screen.anchoredPosition = offScreenPosition;
         ShowShopScreen();
